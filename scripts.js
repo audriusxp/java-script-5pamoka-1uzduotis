27a00852bc0d8cc46ccda0f59dbadd29b1c2cffe
audriusxp <palm@inbox.lt>
2022-06-14 19:29:32 +0300

    alikta 1 dalis uzduoties ,gauta suma masyvo

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
-window.onload = function(){
-    
+window.onload = function() 
+{
+const testinis = [3,5,2,1];
+console.log(testinis);
+let suma = testinis[0] + testinis[1] + testinis[2] + testinis[3];
+console.log(suma);
+
 }
