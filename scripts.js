9667f1e04674c3877575f0a2bc6cc6016aad60f1
audriusxp <palm@inbox.lt>
2022-06-15 08:30:28 +0300

    teoriskai atlikta uzduotis bet turiu abejoniu del rezltato

--- scripts.js
+++ scripts.js
@@ -2,7 +2,19 @@ window.onload = function()
 {
 const testinis = [3,5,2,1];
 console.log(testinis);
-let suma = testinis[0] + testinis[1] + testinis[2] + testinis[3];
+let suma =masyvoSuma(testinis);
 console.log(suma);
 
+
+}  
+
+
+ 
+function masyvoSuma(testinis) {
+    let suma = 0;
+    for (let i = 0; i < testinis.length; i++) {
+       
+        suma = (suma + testinis[i]) % testinis.length;       
+    }
+    return suma;
 }
