da1906a7f7cdfb36fe08c384d838e97c88d6106c
audriusxp <palm@inbox.lt>
2022-06-15 12:40:12 +0300

    pataisyta is esmes, prideta nauja funkcija

--- scripts.js
+++ scripts.js
@@ -3,19 +3,29 @@ window.onload = function()
 const testinis = [3,5,2,1];
 console.log(testinis);
 let suma =masyvoSuma(testinis);
+console.log(suma);
+let vidurkis =masyvoVidurkis(testinis);
 console.log(vidurkis);
 
 
 }  
-
-
- 
 function masyvoSuma(testinis) {
     let suma = 0;
     for (let i = 0; i < testinis.length; i++) {
-       
-        suma = suma + testinis[i] ; 
-        vidurkis = suma / testinis.length;      
+        
+        suma += testinis[i];  
+              
     }
-    return vidurkis;
+    return suma;
 }
+function masyvoVidurkis(testinis){
+    
+        vidurkis =  masyvoSuma(testinis) / testinis.length;
+        
+
+        return vidurkis;
+              
+    
+    
+}
+
